c24e170e61276eeec703b8a62ca850b9d1f5e0ed
alfikgg <alfikgg@gmail.com>
2020-10-06 14:01:03 +0300

    First pull
    First pull

---
+++ Assets/Scripts/AlarmZone.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR.Interaction;
+
+public class AlarmZone : MonoBehaviour
+{
+    [SerializeField] private GameObject _box;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private float _startAlarmVolume;
+    
+    private bool _alarmOn;
+
+    private void Start()
+    {
+        _audioSource.volume = _startAlarmVolume;
+        _alarmOn = false;
+    }
+
+    private void Update()
+    {
+        if(_alarmOn)
+            StartCoroutine(AlarmVolumeUp());
+        if (!_alarmOn && _audioSource.volume > 0)
+            StartCoroutine(AlarmVolumeDown());
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent<Thief>(out Thief thief))
+        {
+            _box.SetActive(false);
+            _alarmOn = true;
+            _audioSource.Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _box.SetActive(true);
+        _alarmOn = false;
+        if (_audioSource.volume <= 0)
+            _audioSource.Stop();
+    }
+
+    private IEnumerator AlarmVolumeUp()
+    {
+        for (float i = 0; i <= 1; i++)
+        {
+            _audioSource.volume += i * Time.deltaTime;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator AlarmVolumeDown()
+    {
+        for (float i = 0; i <= 1; i++)
+        {
+            _audioSource.volume -= i * Time.deltaTime;
+
+            yield return null;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerControll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5;
+    
+    private Rigidbody2D _body;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _body = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        _body.velocity = new Vector2(horizontal * _speed, _body.transform.position.y);
+        if (_body.velocity.x > 0 || _body.velocity.x < 0)
+        {
+            _animator.SetBool("Run", true);
+        }
+        else
+        {
+            _animator.SetBool("Run", false);
+        }
+    }
+}
